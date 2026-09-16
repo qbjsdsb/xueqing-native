330d0940ff23e518f52afd933b42a1eb79e58081
qbjsdsb <3342513714@qq.com>
2026-09-16 21:04:23 +0800

    refactor(android): keep Activity as platform bootstrap only

--- apps/android/app/src/main/java/com/xueqing/app/MainActivity.kt
+++ apps/android/app/src/main/java/com/xueqing/app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.xueqing.app.presentation.XueqingApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
