ffc9c24d6514f008901d504cea2e563a07ed97e9
qbjsdsb <3342513714@qq.com>
2026-09-23 13:10:12 +0800

    Wire Windows workspace commands through compatibility gate

--- apps/windows/src/Xueqing.Windows/Integration/ProviderTeachingWorkspaceBuilder.cs
+++ apps/windows/src/Xueqing.Windows/Integration/ProviderTeachingWorkspaceBuilder.cs
@@ -12,6 +12,7 @@ internal static class ProviderTeachingWorkspaceBuilder
         Uri projectUri,
         string apiKey,
         Func<CancellationToken, ValueTask<string?>> accessTokenProvider,
+        IConsequentialWriteCompatibilityGate compatibilityGate,
         string environmentId,
         ApplicationData applicationData,
         string? functionRegion = null)
@@ -19,40 +20,61 @@ internal static class ProviderTeachingWorkspaceBuilder
         ArgumentNullException.ThrowIfNull(httpClient);
         ArgumentNullException.ThrowIfNull(projectUri);
         ArgumentNullException.ThrowIfNull(accessTokenProvider);
+        ArgumentNullException.ThrowIfNull(compatibilityGate);
         ArgumentNullException.ThrowIfNull(applicationData);
         ArgumentException.ThrowIfNullOrWhiteSpace(apiKey);
         ArgumentException.ThrowIfNullOrWhiteSpace(environmentId);
 
         var bootstrapReader = new PostgrestPersonalBootstrapReader(httpClient, projectUri, apiKey, accessTokenProvider);
         var organizationManagementReader = new PostgrestOrganizationManagementReader(httpClient, projectUri, apiKey, accessTokenProvider);
-        var organizationInvitationCommand = new PostgrestOrganizationInvitationCommand(httpClient, projectUri, apiKey, accessTokenProvider);
-        var organizationInvitationAcceptance = new PostgrestAcceptOrganizationInvitationCommand(httpClient, projectUri, apiKey, accessTokenProvider);
-        var organizationInvitationDelivery = new SupabaseOrganizationInvitationDeliveryCommand(
-            httpClient, projectUri, apiKey, accessTokenProvider, functionRegion);
+        var organizationInvitationCommand = new CompatibilityGatedOrganizationInvitationCommand(
+            new PostgrestOrganizationInvitationCommand(httpClient, projectUri, apiKey, accessTokenProvider),
+            compatibilityGate);
+        var organizationInvitationAcceptance = new CompatibilityGatedAcceptOrganizationInvitationCommand(
+            new PostgrestAcceptOrganizationInvitationCommand(httpClient, projectUri, apiKey, accessTokenProvider),
+            compatibilityGate);
+        var organizationInvitationDelivery = new CompatibilityGatedOrganizationInvitationDeliveryCommand(
+            new SupabaseOrganizationInvitationDeliveryCommand(
+                httpClient, projectUri, apiKey, accessTokenProvider, functionRegion),
+            compatibilityGate);
         var organizationInvitationRecovery = new WindowsOrganizationInvitationRecoveryStore(environmentId, applicationData);
         var recentReader = new PostgrestStudentRecentObservationsReader(httpClient, projectUri, apiKey, accessTokenProvider);
         var focusReader = new PostgrestStudentLearningFocusReader(httpClient, projectUri, apiKey, accessTokenProvider);
         var caseHistoryReader = new PostgrestStudentLearningCasesReader(httpClient, projectUri, apiKey, accessTokenProvider);
         var todayReader = new PostgrestPersonalTodayActionsReader(httpClient, projectUri, apiKey, accessTokenProvider);
-        var createLearningCase = new PostgrestCreateLearningCaseCommand(httpClient, projectUri, apiKey, accessTokenProvider);
+        var createLearningCase = new CompatibilityGatedCreateLearningCaseCommand(
+            new PostgrestCreateLearningCaseCommand(httpClient, projectUri, apiKey, accessTokenProvider),
+            compatibilityGate);
         var createLearningCaseRecovery = new WindowsCreateLearningCaseRecoveryStore(environmentId, applicationData);
 
         var observationCapture = new ObservationQuickCaptureCoordinator(
             new WindowsObservationDraftStore(environmentId, applicationData),
             new WindowsCreateObservationRecoveryStore(environmentId, applicationData),
-            new PostgrestCreateObservationCommand(httpClient, projectUri, apiKey, accessTokenProvider));
+            new CompatibilityGatedCreateObservationCommand(
+                new PostgrestCreateObservationCommand(httpClient, projectUri, apiKey, accessTokenProvider),
+                compatibilityGate));
 
         var actionProgressionRecovery = new WindowsActionProgressionRecoveryStore(environmentId, applicationData);
         var actionProgression = new ActionProgressionCommandCoordinator(
-            new PostgrestReschedulePrimaryActionCommand(httpClient, projectUri, apiKey, accessTokenProvider),
-            new PostgrestRecordVerificationAndNextActionCommand(httpClient, projectUri, apiKey, accessTokenProvider),
+            new CompatibilityGatedReschedulePrimaryActionCommand(
+                new PostgrestReschedulePrimaryActionCommand(httpClient, projectUri, apiKey, accessTokenProvider),
+                compatibilityGate),
+            new CompatibilityGatedRecordVerificationAndNextActionCommand(
+                new PostgrestRecordVerificationAndNextActionCommand(httpClient, projectUri, apiKey, accessTokenProvider),
+                compatibilityGate),
             actionProgressionRecovery);
 
         var caseLifecycleRecovery = new WindowsCaseLifecycleRecoveryStore(environmentId, applicationData);
         var caseLifecycle = new CaseLifecycleCommandCoordinator(
-            new PostgrestTransitionLearningCaseStateCommand(httpClient, projectUri, apiKey, accessTokenProvider),
-            new PostgrestCloseLearningCaseCommand(httpClient, projectUri, apiKey, accessTokenProvider),
-            new PostgrestReopenLearningCaseCommand(httpClient, projectUri, apiKey, accessTokenProvider),
+            new CompatibilityGatedTransitionLearningCaseStateCommand(
+                new PostgrestTransitionLearningCaseStateCommand(httpClient, projectUri, apiKey, accessTokenProvider),
+                compatibilityGate),
+            new CompatibilityGatedCloseLearningCaseCommand(
+                new PostgrestCloseLearningCaseCommand(httpClient, projectUri, apiKey, accessTokenProvider),
+                compatibilityGate),
+            new CompatibilityGatedReopenLearningCaseCommand(
+                new PostgrestReopenLearningCaseCommand(httpClient, projectUri, apiKey, accessTokenProvider),
+                compatibilityGate),
             caseLifecycleRecovery);
 
         return new PersonalTeachingWorkspaceServices(
